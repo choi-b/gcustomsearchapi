55cbb669e158bcd5944c93f2043913efdd138d64
Brian <brian.ks.choi@gmail.com>
2023-02-11 04:08:36 -0500

    updated settings.py 2.11.2023

--- settings.py
+++ settings.py
@@ -1,5 +1,7 @@
-SEARCH_KEY = "key"
-SEARCH_ID = "id"
+import private
+
+SEARCH_KEY = private.api
+SEARCH_ID = private.engine_id
 COUNTRY = "us"
 
 SEARCH_URL = "https://www.googleapis.com/customsearch/v1?key={key}&cx={cx}&q={query}&start={start}&gl=" + COUNTRY
