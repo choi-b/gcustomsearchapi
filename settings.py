0b6102c08eeff0e84ab79c48422cecf526369326
Brian <brian.ks.choi@gmail.com>
2023-02-11 04:05:51 -0500

    initial set up 2.11.2023

---
+++ settings.py
@@ -0,0 +1,6 @@
+SEARCH_KEY = "key"
+SEARCH_ID = "id"
+COUNTRY = "us"
+
+SEARCH_URL = "https://www.googleapis.com/customsearch/v1?key={key}&cx={cx}&q={query}&start={start}&gl=" + COUNTRY
+RESULT_COUNT = 20
